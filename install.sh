c8629cf63977b1a79e9c3cdbf81d7ec824add079
Bin Zhou <zhoubin.me@gmail.com>
2021-11-13 21:29:57 +0800

    .

---
+++ install.sh
@@ -0,0 +1,2 @@
+npm install hexo-render-pug hexo-renderer-stylus hexo-algolia hexo-wordcount crypto-js  hexo-wordcount hexo-generator-feed hexo-abbrlink
+git clone -b dev https://github.com/YunYouJun/hexo-theme-yun themes/yun
